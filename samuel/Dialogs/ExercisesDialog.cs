6e446824cafbe89c9c4ed543d78f48f7b9643e1f
quispewilmer <wilmerquispecode@gmail.com>
2023-06-18 00:45:43 -0500

    Almost complete themes section

--- samuel/Dialogs/ExercisesDialog.cs
+++ samuel/Dialogs/ExercisesDialog.cs
@@ -2,23 +2,29 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Data.Tables;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
-using Microsoft.BotBuilderSamples;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Extensions.Configuration;
+using samuel.Data;
 
 namespace samuel.Dialogs
 {
     public class ExercisesDialog : ComponentDialog
     {
+        private readonly IConfiguration _config;
+        TableServiceClient tableServiceClient;
 
-        public ExercisesDialog()
+        public ExercisesDialog(IConfiguration config)
             : base(nameof(ExercisesDialog))
         {
+            _config = config;
+            tableServiceClient = new TableServiceClient(_config["Storage:ConnectionString"]);
+
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
             {
                 InitialStepAsync,
@@ -30,13 +36,34 @@ namespace samuel.Dialogs
 
         private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            var promptOptions = new PromptOptions { Prompt = MessageFactory.Text("Please enter your name.") };
-            return await stepContext.PromptAsync(nameof(TextPrompt), promptOptions, cancellationToken);
+            TableClient tableClient = tableServiceClient.GetTableClient(
+                tableName: _config["Storage:Exercices:TableName"]
+            );
+            List<Attachment> attachments = new List<Attachment>();
+            IEnumerable<Exercice> exercices = tableClient.Query<Exercice>();
+            foreach (var exercice in exercices.ToList())
+            {
+                attachments.Add(
+                    new HeroCard(
+                        title: exercice.Name,
+                        text: exercice.Content,
+                        tap: new CardAction()
+                        {
+                            Type = ActionTypes.MessageBack,
+                            Value = exercice.Id
+                        }
+                    )
+                    .ToAttachment()
+                );
+            }
+            var activity = MessageFactory.Carousel(attachments);
+            await stepContext.Context.SendActivityAsync(activity);
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
         }
 
         private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            return await stepContext.EndDialogAsync(null, cancellationToken);
+            return await stepContext.ContinueDialogAsync(cancellationToken);
         }
     }
 }
